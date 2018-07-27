1376c2bfe84979f58dd82e5aad853158128bc167
guangyuanliu@gmail.com <guangyuanliu@gmail.com>
2018-07-26 23:23:52 -0400

    Added new examples

--- initial/src/main/java/hello/HelloController.java
+++ initial/src/main/java/hello/HelloController.java
@@ -1,14 +1,26 @@
 package hello;
 
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.concurrent.atomic.AtomicLong;
+
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 @RestController
 public class HelloController {
     
+	private static final String template = "Hello, %s!";
+	private final AtomicLong counter = new AtomicLong();
+	
     @RequestMapping("/")
     public String index() {
         return "Greetings from Spring Boot!";
     }
     
+    @RequestMapping("/hello-world")
+    public Greeting sayHello(@RequestParam(name="name", required = false, defaultValue = "Stranger") 
+    							String name) {
+    	return new Greeting(counter.incrementAndGet(), String.format(template, name));
+    }
 }
--- initial/src/test/java/hello/HelloControllerHTTPTest.java
+++ initial/src/test/java/hello/HelloControllerHTTPTest.java
@@ -4,6 +4,7 @@ import static org.hamcrest.CoreMatchers.equalTo;
 import static org.junit.Assert.assertThat;
 
 import java.net.URL;
+import java.util.Map;
 
 import org.junit.Before;
 import org.junit.Test;
@@ -12,8 +13,10 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.web.client.TestRestTemplate;
 import org.springframework.boot.web.server.LocalServerPort;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.test.context.junit4.SpringRunner;
+import static org.assertj.core.api.BDDAssertions.then;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
@@ -37,4 +40,28 @@ public class HelloControllerHTTPTest {
 		ResponseEntity<String> response = template.getForEntity(base.toString(), String.class);
 		assertThat(response.getBody(), equalTo("Greetings from Spring Boot!"));
 	}
+	
+	@Test
+	public void shouldReturn200WhenSendingRequestToController() throws Exception {
+		String defaultName = "Hello, Stranger!";
+		@SuppressWarnings("rawtypes")
+		ResponseEntity<Map> entity = this.template.getForEntity(
+				base.toString() + "/hello-world", Map.class);
+
+		then(entity.getStatusCode()).isEqualTo(HttpStatus.OK);
+		assertThat(entity.getBody().get("content"), equalTo(defaultName));
+	}
+
+	@Test
+	public void shouldReturnNameWhenSendingRequestToController() throws Exception {
+		
+		String name = "Happy_User";
+		@SuppressWarnings("rawtypes")
+		ResponseEntity<Map> entity = this.template.getForEntity(
+				base.toString() + "/hello-world?name="+name, Map.class);
+
+		then(entity.getStatusCode()).isEqualTo(HttpStatus.OK);
+		assertThat(entity.getBody().get("content"), equalTo(String.format("Hello, %s!", name)));
+	}
+	
 }
